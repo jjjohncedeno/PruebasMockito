ff7168fbd347d7fac1ef88dc6f265f6141d17934
John <jjjohncedeno@gmail.com>
2016-12-23 11:17:23 -0500

    Creacion de la clase Customer. Se ha creado la clase customer y la interfaz Validation.

---
+++ src/main/java/Customer.java
@@ -0,0 +1,49 @@
+import interfaces.Validation;
+
+public class Customer implements Validation {
+	private int edad;
+	private String nombre;
+	private String cedula;
+	
+	public Boolean isAdult() {
+		// TODO Auto-generated method stub
+		if (this.edad < 18){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public Customer(int edad, String nombre, String cedula) {
+		super();
+		this.edad = edad;
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+	
+	
+}
