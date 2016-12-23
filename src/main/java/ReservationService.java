0cd6d90b5d40f44a70550c247ba6cd98b56d8a33
Administrador <Administrador@GGIECSOFWRK139.espol.edu.ec>
2016-12-23 12:01:51 -0500

    Creacion de Reservation Service

---
+++ src/main/java/ReservationService.java
@@ -0,0 +1,20 @@
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+
+public class ReservationService {
+
+	public ReservationService(){
+			
+	}
+	
+	public boolean isRentableUntil(Movie movie, Date date){
+	
+		return false;
+	}
+	
+}
+
