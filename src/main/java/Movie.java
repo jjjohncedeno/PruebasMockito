6b5a795dc6848d8668792a1d528260f6fa324b5d
luchoadp <luchoadp93@gmail.com>
2016-12-23 12:20:18 -0500

    clase Movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,4 +1,14 @@
 
 public class Movie {
-
+	private String nombre;
+	private Boolean estado;
+	public Movie(String nombre, Boolean estado) {
+		super();
+		this.nombre = nombre;
+		this.estado = estado;
+	}
+	public boolean isRentableTo(Customer customer){
+		return customer.isAdult();
+	}
+	
 }
